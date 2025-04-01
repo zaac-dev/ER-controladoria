88e998ae0dc7e91c9ac35c2bfc54ddfd12ff1f10
Zaac-dev <zaacdesenvolvedorweb@gmail.com>
2025-04-01 18:24:02 -0300

    ajustes finais

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 const hamburguer = document.querySelector('.hamburger');
 const navList = document.querySelector('.nav');
+const navItems = document.querySelectorAll('.nav li'); 
+
 
 hamburguer.addEventListener('click', () => 
     navList.classList.toggle('active'));
 
+
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        navList.classList.remove('active'); 
+    });
+});
+
+
 function openModal(articleId) {
     document.getElementById(articleId + 'Modal').style.display = 'flex';
     document.body.style.overflow = 'hidden';
-  }
+}
   
-  function closeModal(articleId) {
+function closeModal(articleId) {
     document.getElementById(articleId + 'Modal').style.display = 'none';
     document.body.style.overflow = 'auto';
-  }
+}
   
-  // Fechar o modal ao clicar fora do conteúdo
-  window.onclick = function(event) {
+
+window.onclick = function(event) {
     const modals = document.getElementsByClassName('modal');
     for (let i = 0; i < modals.length; i++) {
-      if (event.target === modals[i]) {
-        modals[i].style.display = 'none';
-        document.body.style.overflow = 'auto';
-      }
+        if (event.target === modals[i]) {
+            modals[i].style.display = 'none';
+            document.body.style.overflow = 'auto';
+        }
     }
-  }
+}
