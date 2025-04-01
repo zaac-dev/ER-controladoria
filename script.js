f1667fc5d802aa2279d30896750c9f6713cb9f43
Zaac-dev <zaacdesenvolvedorweb@gmail.com>
2025-03-31 21:30:48 -0300

    fase teste

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ const navList = document.querySelector('.nav');
 
 hamburguer.addEventListener('click', () => 
     navList.classList.toggle('active'));
+
+function openModal(articleId) {
+    document.getElementById(articleId + 'Modal').style.display = 'flex';
+    document.body.style.overflow = 'hidden';
+  }
+  
+  function closeModal(articleId) {
+    document.getElementById(articleId + 'Modal').style.display = 'none';
+    document.body.style.overflow = 'auto';
+  }
+  
+  // Fechar o modal ao clicar fora do conteúdo
+  window.onclick = function(event) {
+    const modals = document.getElementsByClassName('modal');
+    for (let i = 0; i < modals.length; i++) {
+      if (event.target === modals[i]) {
+        modals[i].style.display = 'none';
+        document.body.style.overflow = 'auto';
+      }
+    }
+  }
